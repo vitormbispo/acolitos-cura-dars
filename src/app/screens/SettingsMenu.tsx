f259b976aede155a00ec363372c046ef8070d510
vitormartbispo@gmail.com <vitormartbispo@gmail.com>
2025-03-04 14:19:37 -0300

    Implementando ExpoDocumentPicker

--- src/app/screens/SettingsMenu.tsx
+++ src/app/screens/SettingsMenu.tsx
@@ -2,6 +2,7 @@ import { View } from "react-native";
 import { LinkRowImageButton, RowImageButton, UpperBar } from "../classes/NewComps";
 import { ICONS } from "../classes/AssetManager";
 import * as FileSystem from 'expo-file-system'
+import * as DocumentPicker from 'expo-document-picker'
 import { MemberData } from "../classes/MemberData";
 export default function SettingsMenu(){
     return(
@@ -9,6 +10,9 @@ export default function SettingsMenu(){
             <UpperBar icon={ICONS.menu} screenName={"Menu"}/>
             <RowImageButton img={ICONS.acolito} text={"Exportar dados"} press={()=>{
                 SaveDataFile("Data","application/json")
+            }}/>
+            <RowImageButton img={ICONS.acolito} text={"Importar dados"} press={()=>{
+                LoadDataFile()
             }}/>
         </View>
     )
@@ -32,4 +36,14 @@ async function SaveDataFile(name:string,mimetype:string){
             await FileSystem.writeAsStringAsync(uri,JSON.stringify(data),{encoding: FileSystem.EncodingType.UTF8})
         })
     }
+}
+
+async function LoadDataFile() {
+    let doc = DocumentPicker.getDocumentAsync({type:"application/json",copyToCacheDirectory:true,multiple:false})
+        .then(
+            (result)=>{return result.assets[0]}
+    )
+
+    console.log(doc)
+    
 }
